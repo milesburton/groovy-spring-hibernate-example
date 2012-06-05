119d58b91d01d561b9e13cc4fa9552796e344308
Miles Burton <miles@mnetcs.com>
2012-06-05 20:32:20 +0100

    first commit

---
+++ src/main/groovy/com/mb/example/hibernate/Main.groovy
@@ -0,0 +1,11 @@
+package com.mb.example.hibernate
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: miles
+ * Date: 05/06/12
+ * Time: 16:54
+ * To change this template use File | Settings | File Templates.
+ */
+class Main {
+}
---
+++ src/main/groovy/com/mb/example/hibernate/domain/Book.groovy
@@ -0,0 +1,11 @@
+package com.mb.example.hibernate.domain
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: miles
+ * Date: 05/06/12
+ * Time: 16:13
+ * To change this template use File | Settings | File Templates.
+ */
+class Book {
+}
